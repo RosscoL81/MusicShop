4fe4c0d7f3bfec1df74763c0e5f7a4129ed867f7
Ross Lorimer <wross.lorimer@googlemail.com>
2019-11-29 21:58:42 +0000

    profit method added

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,44 @@
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private String name;
+    private ArrayList<ISell> stock;
+    private double till;
+
+    public Shop(String name, double till){
+        this.name = name;
+        this.till = till;
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getTill() {
+        return this.till;
+    }
+
+    public void addStock(ISell item){
+        this.stock.add(item);
+    }
+
+    public int getStockCount() {
+        return stock.size();
+    }
+
+    public void removeStock(ISell item) {
+        this.stock.remove(item);
+    }
+
+    public double getPotentialProfit() {
+        double profit = 0;
+        for (ISell item : this.stock) {
+            profit += item.calculateMarkUp();
+        }
+        return profit;
+    }
 }
---
+++ src/test/java/ShopTest.java
@@ -0,0 +1,62 @@
+import behaviours.ISell;
+import instruments.FamilyType;
+import instruments.Guitar;
+import items.DrumSticks;
+import items.SheetMusic;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShopTest {
+
+    Shop shop;
+    ISell guitar;
+    ISell sheetMusic;
+    ISell drumSticks;
+    ISell guitar2;
+
+    @Before
+    public void before(){
+        shop = new Shop("Ray's Music Exchange", 1000.00);
+        guitar = new Guitar(FamilyType.STRING, "wood", 100, 120, 4, "bass");
+        sheetMusic = new SheetMusic("Stereophonics Sheet Music", 5.00, 7.99);
+        drumSticks = new DrumSticks("Hickory", 0.99, 3.99);
+        guitar2 = new Guitar(FamilyType.STRING, "wood", 150.00, 225.00, 6, "acoustic");
+
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Ray's Music Exchange", shop.getName());
+    }
+
+    @Test
+    public void canGetTill(){
+        assertEquals(1000.00, shop.getTill(), 0.01);
+    }
+
+    @Test
+    public void canAddStock(){
+        shop.addStock(guitar);
+        assertEquals(1, shop.getStockCount());
+    }
+
+    @Test
+    public void canRemoveStock(){
+        shop.addStock(guitar);
+        shop.addStock(sheetMusic);
+        shop.removeStock(guitar);
+        assertEquals(1, shop.getStockCount());
+    }
+
+    @Test
+    public void canGetTotalProfitOfStock(){
+        shop.addStock(guitar);
+        shop.addStock(sheetMusic);
+        shop.addStock(drumSticks);
+        shop.addStock(guitar2);
+        assertEquals(100.99, shop.getPotentialProfit(), 0.01);
+
+    }
+}
